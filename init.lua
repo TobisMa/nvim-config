fd9c6ef89332af4648775d71b9f21c7dba1250f2
TobisMa <tobias-ma@gmx.net>
2025-09-11 01:25:11 +0200

    remove unnecessary :make files

--- init.lua
+++ init.lua
@@ -2,7 +2,6 @@
 
 local options = require('options')
 local keymap = require('keys')
-local makes = require('make')
 local lazyvim = require('lazyvim')
 local colors = require('coloring')
 local lsp = require('lsp')
