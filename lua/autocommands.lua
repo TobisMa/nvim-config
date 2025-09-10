d77984fb0d8b0ed5761727676f54a76c1e111cd5
TobisMa <tobias-ma@gmx.net>
2025-09-11 01:24:38 +0200

    small autocommand fixes

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -1,3 +1,4 @@
+-- START general
 vim.api.nvim_create_autocmd('TextYankPost', {
     desc = 'Highlight when yanking (copying) text',
     group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
@@ -12,19 +13,12 @@ vim.api.nvim_create_autocmd("InsertEnter", {
 })
 
 vim.api.nvim_create_autocmd("BufWritePre", {
-    desc = "Replace tabs with 4 spaces",
-    group = vim.api.nvim_create_augroup("replace-tab-with-space", {clear=true}),
-    pattern = {"*.java"},
-    command = "silent! %s/     /    /g",
-})
-
-vim.api.nvim_create_autocmd("BufWritePre", {
     desc = "Remove trailing whitespace (excluing line from cursor if it is a whitespace line)",
     group = vim.api.nvim_create_augroup("remove-trailing", {clear=true}),
     pattern = {"*"},
     callback = function ()
         local line = vim.api.nvim_get_current_line()
-        vim.cmd[[silent! %s/\s\+$//]]
+        vim.cmd[[silent! %s/\(\S\)\s\+$/\1/]]
         if line:match("^%s*$") then
             vim.api.nvim_set_current_line(line)
         end
@@ -32,12 +26,12 @@ vim.api.nvim_create_autocmd("BufWritePre", {
     end,
 })
 
-
 vim.api.nvim_create_autocmd("BufNew", {
     desc = "load lsp keymaps",
-    group = vim.api.nvim_create_augroup("lsp-mappins", {clear=true}),
+    group = vim.api.nvim_create_augroup("lsp-mappings", {clear=true}),
     pattern = {"*"},
     callback = function()
         lsp_mappings()
     end,
 })
+-- END general
