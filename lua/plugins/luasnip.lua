76023b1792a636c79214c5f7b826d1fedceeb906
TobisMa <tobias-ma@gmx.net>
2025-09-10 20:25:10 +0200

    attempt for smarter ctrl-e

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -5,7 +5,13 @@ return {
         require("luasnip").setup({ enable_autosnippets = true })
         require("luasnip.loaders.from_lua").lazy_load({ paths = "~/.config/nvim/snippets/" })
         local ls = require("luasnip")
-        vim.keymap.set("i", "<C-e>", function() ls.expand_or_jump(1) end, { silent = true })
+
+        local function smart_expand()
+            if vim.fn.pumvisible() == 0 then
+                return ls.expand_or_jump(1)
+            end
+        end
+        vim.keymap.set("i", "<C-e>", smart_expand, {silent=true, expr=true, noremap=false})
         vim.keymap.set({ "i", "s" }, "<C-J>", function() ls.jump(1) end, { silent = true })
         vim.keymap.set({ "i", "s" }, "<C-K>", function() ls.jump(-1) end, { silent = true })
     end
