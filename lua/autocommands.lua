b6ded6246e184507969fd0610542f8225cf88150
TobisMa <tobias-ma@gmx.net>
2025-09-13 00:02:07 +0200

    format autocommand file

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -2,7 +2,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     desc = 'Highlight when yanking (copying) text',
     group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
     callback = function()
-        vim.highlight.on_yank({ timeout=50 })
+        vim.highlight.on_yank({ timeout = 50 })
     end,
 })
 
@@ -13,18 +13,14 @@ vim.api.nvim_create_autocmd("InsertEnter", {
 
 vim.api.nvim_create_autocmd("BufWritePre", {
     desc = "Remove trailing whitespace (excluing line from cursor if it is a whitespace line)",
-    group = vim.api.nvim_create_augroup("remove-trailing", {clear=true}),
-    pattern = {"*"},
-    callback = function ()
+    group = vim.api.nvim_create_augroup("remove-trailing", { clear = true }),
+    pattern = { "*" },
+    callback = function()
         local line = vim.api.nvim_get_current_line()
-        vim.cmd[[silent! %s/\(\S\)\s\+$/\1/]]
+        vim.cmd [[silent! %s/\(\S\)\s\+$/\1/]]
         if line:match("^%s*$") then
             vim.api.nvim_set_current_line(line)
         end
-        vim.cmd[[normal $]]
+        vim.cmd [[normal $]]
     end,
 })
--- vim.api.nvim_buf_set_extmark(0, vim.api.nvim_create_namespace("indent-guides"), 33, 0, {virt_text={{"│", "indent-guides"}}, virt_text_win_col=4})
--- vim.api.nvim_buf_del_extmark(0,
-
-
