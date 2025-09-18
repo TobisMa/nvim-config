4cb70f4b00743478fd2b079d7b03d424740276fc
TobisMa <tobias-ma@gmx.net>
2025-09-18 23:34:30 +0200

    update netrw config

--- lua/netrw.lua
+++ lua/netrw.lua
@@ -3,7 +3,6 @@ vim.g.netrw_browse_split = 0
 vim.g.netrw_liststyle = 3  -- cycle by pressing i
 vim.g.netrw_fastbrowse = 2  -- update directory with <C-l>
 vim.g.netrw_special_syntax = 1
-vim.g.netrw_winsize = -40
+vim.g.netrw_winsize = -35
 
 
-vim.keymap.set("n", "<leader><leader>f", "<cmd>Lexplore<cr>")
