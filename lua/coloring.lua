9d8312815a1196a9fd847261cc531a7c14698c2d
TobisMa <tobias-ma@gmx.net>
2025-11-01 20:42:42 +0100

    coloring for completion and LSP inlay hints

--- lua/coloring.lua
+++ lua/coloring.lua
@@ -2,9 +2,14 @@
 vim.cmd[[
 colorscheme vague
 hi Search ctermfg=0 ctermbg=11 guifg=#cdcdcd guibg=#405065
+
 hi NonText guifg=red
-hi PreInsert cterm=bold
-hi ColorColumn ctermbg=black
 hi Whitespace guifg=#a5a5a5
+"hi PreInsert cterm=bold
+hi ColorColumn ctermbg=black
+
+hi ComplMatchIns gui=NONE guifg=#777777
+hi LspInlayHint gui=italic guifg=#777777
 ]]
+
 
