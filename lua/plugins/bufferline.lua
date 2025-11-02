a505e3c6e77377440a5584f21d90fd9690e7c295
TobisMa <tobias-ma@gmx.net>
2025-11-03 00:51:16 +0100

    use ordinal number for buffers and include bufnr to file

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -12,8 +12,18 @@ return {
             show_tab_indicators = true,
             -- separator_style = "thin",
             numbers = function(opts)
-                return opts.id
+                return opts.ordinal
             end,
+            name_formatter = function (buf)
+                local max_length = 22 - tostring(buf.bufnr):len()
+                local name = buf.name
+                if name:len() > max_length then
+                    name = name:sub(1, max_length - 1) .. "…"
+                end
+                return string.format("%s (%s)", name, buf.bufnr)
+            end,
+            max_name_length=25,
+            diagnostics = "nvim_lsp"
         },
     }
 }
