17d2a3cc353897c0eec74d0cd23887137cce8726
TobisMa <tobias-ma@gmx.net>
2025-11-03 14:11:29 +0100

    sync formatoption in after/ftplugin/lua.lua with options default

--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -1,1 +1,1 @@
-vim.opt.formatoptions = "tcrlvj1q/n"  -- for some reason that option is overwritten all the time
+vim.opt.formatoptions =  "crlvj1q/n" -- for some reason that option is overwritten all the time
