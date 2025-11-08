537efd7dcf14dbe3903a6cd7c225c12bebfe194d
TobisMa <tobias-ma@gmx.net>
2025-11-08 22:51:22 +0100

    fixed weird indentation removal for jdtls/java files

--- after/ftplugin/java.lua
+++ after/ftplugin/java.lua
@@ -2,5 +2,5 @@ vim.api.nvim_create_autocmd("BufWritePre", {
     desc = "Replace tabs with 4 spaces",
     group = vim.api.nvim_create_augroup("replace-tab-with-space", {clear=true}),
     pattern = {"*.java"},
-    command = "silent! %s/     /    /g",
+    command = "silent! %s/\t/    /g",
 })
