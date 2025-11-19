49ae11bb97e3bd03df9de055935b63163bc809ad
TobisMa <tobias-ma@gmx.net>
2025-11-19 21:07:55 +0100

    fixed indentation

--- after/lsp/texlab.lua
+++ after/lsp/texlab.lua
@@ -1,24 +1,24 @@
 return {
-  settings = {
-    texlab = {
-      build = {
-        executable = 'latexmk',
-        args = { '-pdf', '-interaction=nonstopmode', '-synctex=1', '%f' },
-        onSave = false,
-        forwardSearchAfter = false,
-      },
-      chktex = {
-        onOpenAndSave = true,
-        onEdit = true,
-      },
-      diagnosticsDelay = 200,
-      latexFormatter = 'latexindent',
-      latexindent = {
-        ['local'] = nil, -- local is a reserved keyword
-        modifyLineBreaks = false,
-      },
-      bibtexFormatter = 'texlab',
-      formatterLineLength = 80,
+    settings = {
+        texlab = {
+            build = {
+                executable = 'latexmk',
+                args = { '-pdf', '-interaction=nonstopmode', '-synctex=1', '%f' },
+                onSave = false,
+                forwardSearchAfter = false,
+            },
+            chktex = {
+                onOpenAndSave = true,
+                onEdit = true,
+            },
+            diagnosticsDelay = 200,
+            latexFormatter = 'latexindent',
+            latexindent = {
+                ['local'] = nil, -- local is a reserved keyword
+                modifyLineBreaks = false,
+            },
+            bibtexFormatter = 'texlab',
+            formatterLineLength = 80,
+        },
     },
-  },
 }
